3a04e16f18bdbfb15bd6b51a509a15a7f88a5b58
Bhawana <bhwbi.rawal@gmail.com>
2025-05-10 12:53:50 +0545

    add project template

---
+++ Devalaya.Explorer/Devalaya.Explorer.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Devalaya.Explorer.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
